e5fd90d48c52518f45a9ad059d5519ab3b5344dc
Vineet John <1531017+vineetjohn@users.noreply.github.com>
2023-05-19 23:25:47 -0700

    Add black and pre-commit as dev dependencies
    Add black and autoflake pre-commit hooks

--- comments_purge.py
+++ comments_purge.py
@@ -1,4 +1,4 @@
-''' Script to overwrite and delete user comments on reddit.com '''
+""" Script to overwrite and delete user comments on reddit.com """
 
 import sys
 import argparse
@@ -6,25 +6,28 @@ import json
 import praw
 
 
-def authenticate_user(reddit_username, reddit_password,
-                      reddit_app_client_id, reddit_app_client_secret):
-    ''' Authenticates user with the PRAW library '''
+def authenticate_user(
+    reddit_username, reddit_password, reddit_app_client_id, reddit_app_client_secret
+):
+    """Authenticates user with the PRAW library"""
 
-    reddit = praw.Reddit(client_id=reddit_app_client_id,
-                         client_secret=reddit_app_client_secret,
-                         username=reddit_username,
-                         password=reddit_password,
-                         user_agent="comments-purger")
+    reddit = praw.Reddit(
+        client_id=reddit_app_client_id,
+        client_secret=reddit_app_client_secret,
+        username=reddit_username,
+        password=reddit_password,
+        user_agent="comments-purger",
+    )
     reddit.validate_on_submit = True
 
     return reddit.user.me() if reddit.user.me() == reddit_username else None
 
 
 def delete_comments(redditor):
-    '''
+    """
     Deletes comments for a reddit user
     reddit: PRAW reddit user instance
-    '''
+    """
 
     for index, comment in enumerate(redditor.comments.new(limit=None)):
         print("Deleting comment {}".format(index))
@@ -33,23 +36,28 @@ def delete_comments(redditor):
 
 
 def main():
-    ''' Main function '''
+    """Main function"""
 
     # Parsing command line args
     parser = argparse.ArgumentParser()
-    parser.add_argument('--credential-file-path', type=str,
-                        help='Path to credential file')
+    parser.add_argument(
+        "--credential-file-path", type=str, help="Path to credential file"
+    )
     options = vars(parser.parse_args())
 
     # Authenticating user
     credentials = None
     print("Reading credentials")
-    with open(options['credential_file_path']) as credential_file:
+    with open(options["credential_file_path"]) as credential_file:
         credentials = json.load(credential_file)
 
     print("Authenticating user")
-    redditor = authenticate_user(credentials['username'], credentials['password'],
-                                 credentials['client-id'], credentials['client-secret'])
+    redditor = authenticate_user(
+        credentials["username"],
+        credentials["password"],
+        credentials["client-id"],
+        credentials["client-secret"],
+    )
     print("Authentication successful")
 
     # Deleting comments
@@ -57,5 +65,6 @@ def main():
     delete_comments(redditor)
     print("Comment deletion completed!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(sys.argv[1:])
